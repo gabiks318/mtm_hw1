619a6c1c64fc2b87f90aa2b72ba0b8062deded76
Gabriel Kositsky <gabiks318@gmail.com>
2020-11-22 18:21:15 +0200

    first question

--- hw1q1.c
+++ hw1q1.c
@@ -2,15 +2,37 @@
 #include <string.h>
 #include <assert.h>
 
-char* stringDuplicator(char* s, int times){
-    assert(!s);
-    assert(times > 0);
-    int LEN = strlen(s);
-    char* out = malloc(LEN*times);
-    assert(out);
-    for (int i=0; i<=times; i++){
-    out = out + LEN;
-    strcpy(out,s);
+char* duplicateString(char* s, int times){
+    assert(s != NULL && times > 0);
+    int len = strlen(s);
+    char* out = malloc(sizeof(char)*(len*times + 1));
+    if(out == NULL)
+    {
+        return;
+    }
+    char* temp = out;
+    for (int i=0; i < times; i++)
+    {
+        strcpy(temp,s);
+        temp += len;
     }
     return out;
 }
+
+
+/* 
+Progarmming Errors: 
+1.#include with "" instead of <>
+2.assert(out)
+3.string created in malloc isn't long enough. we need space for closing the string with '/0'
+4.<= in loop instead of <
+5.out is incremented and when returning it returns the end of the string
+6.change the order between incrementing the pointer and strcpy
+7.assert(s!=NULL) instead of assert(!s)
+
+Convention Errors:
+1.LEN instead of len
+2.function name duplicateString instead of stringDuplicator
+3.for loop doesn't contain spaces
+4.duplicate assert call
+*/
